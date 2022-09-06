417e23d6d8b9b3114f0381b7b2691041c0388fe7
Alena Bartosh <alena.mathematics@gmail.com>
2022-09-06 12:26:09 +0300

    ♻️ HtmlTelegramMessagesParser: Extract to separate file

---
+++ src/html_telegram_messages_parser.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+class HtmlTelegramMessagesParser:
+
+    @staticmethod
+    def parse(file_paths: list[str]) -> pd.DataFrame:
+        """
+        Parse only text messages. Skip pictures, audio, forwarded etc.
+        Return data with the next cols: [date, name, text]
+        """
+        all_messages: list[dict] = []
+
+        for message_file in file_paths:
+            parsed_html = BeautifulSoup(Path(message_file).open('r').read(), features='html.parser')
+
+            part_messages: list[dict] = []
+            last_date: str | None = None
+            last_name: str | None = None
+            is_forwarded: bool | None = None
+
+            needed_attrs: dict[str, list[str]] = {
+                'class': ['forwarded body', 'pull_right date details', 'text', 'from_name']}
+
+            for tag in parsed_html.body('div', attrs=needed_attrs)[1:]:
+                tag_class = tag.attrs['class']
+
+                if 'forwarded' in tag_class:
+                    is_forwarded = True
+                elif 'pull_right' in tag_class:
+                    last_date = tag.attrs['title']
+                elif 'text' in tag_class:
+                    text = tag.text.strip()
+                    if not is_forwarded:
+                        part_messages.append(dict(date=last_date, name=last_name, text=text))
+                    is_forwarded = False
+                elif 'from_name' in tag_class:
+                    last_name = tag.text.strip()
+            all_messages += part_messages
+
+        return pd.DataFrame(all_messages)
--- src/main.py
+++ src/main.py
@@ -7,45 +7,11 @@ from pathlib import Path
 
 import coloredlogs
 import pandas as pd
-from bs4 import BeautifulSoup
+
+from html_telegram_messages_parser import HtmlTelegramMessagesParser
 
 MESSAGES_MASK = "messages*.html"
 DESTINATION_DIR = "dest"
-
-
-def parse_html_messages(file_paths: list[str]) -> pd.DataFrame:
-    """
-    Parse only text messages. Skip pictures, audio, forwarded etc.
-    Return data with the next cols: [date, name, text]
-    """
-    all_messages: list[dict] = []
-
-    for message_file in file_paths:
-        parsed_html = BeautifulSoup(Path(message_file).open('r').read(), features='html.parser')
-
-        part_messages: list[dict] = []
-        last_date: str | None = None
-        last_name: str | None = None
-        is_forwarded: bool | None = None
-
-        for tag in parsed_html.body('div', attrs={
-            'class': ['forwarded body', 'pull_right date details', 'text', 'from_name']})[1:]:
-            tag_class = tag.attrs['class']
-
-            if 'forwarded' in tag_class:
-                is_forwarded = True
-            elif 'pull_right' in tag_class:
-                last_date = tag.attrs['title']
-            elif 'text' in tag_class:
-                text = tag.text.strip()
-                if not is_forwarded:
-                    part_messages.append(dict(date=last_date, name=last_name, text=text))
-                is_forwarded = False
-            elif 'from_name' in tag_class:
-                last_name = tag.text.strip()
-        all_messages += part_messages
-
-    return pd.DataFrame(all_messages)
 
 
 def main() -> None:
@@ -83,7 +49,7 @@ def main() -> None:
 
     logger.debug(f'There are {files_count} files for parsing.')
 
-    messages: pd.DataFrame = parse_html_messages(file_paths)
+    messages: pd.DataFrame = HtmlTelegramMessagesParser.parse(file_paths)
     logger.info(f'Successfully parsed [{messages.shape[0]}] messages.')
 
     senders: list[str] = messages.name.unique()
